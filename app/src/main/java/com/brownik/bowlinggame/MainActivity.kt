d09d6844f5d996c61fcb2d3c847a7c38f2ae3415
pow <vkflskfdk1@naver.com>
2022-12-14 22:23:09 +0900

    설계 변경 및 activity layout 변경

--- app/src/main/java/com/brownik/bowlinggame/MainActivity.kt
+++ app/src/main/java/com/brownik/bowlinggame/MainActivity.kt
@@ -2,12 +2,24 @@ package com.brownik.bowlinggame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.brownik.bowlinggame.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        Calculator
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        addOnClickListener()
+    }
+
+    private fun addOnClickListener() = with(binding) {
+        inputButton.setOnClickListener {
+            val playerCount = inputPlayer.text.toString().toInt()
+            supportFragmentManager
+        }
     }
 }
